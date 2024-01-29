3197495315b33a5d3c69cd7258456af9a11a69ba
zord005 <128481219+zord005@users.noreply.github.com>
2024-01-30 00:30:29 +0700

    Create index.php

---
+++ pelanggan/index.php
@@ -0,0 +1,165 @@
+<?php include 'header.php'; ?>
+
+<section id="hero">
+	<div class="hero-container">
+		<h1>Beat Waves</h1>
+	</div>
+</section>
+
+<!-- Start We Help Section -->
+<div class="we-help-section">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col-lg-7 mb-5 mb-lg-0">
+				<div class="imgs-grid">
+					<div class="grid grid-1"><img src="img/fg800.png" alt="Untree.co"></div>
+					<div class="grid grid-2"><img src="img/drum.png" alt="Untree.co"></div>
+					<div class="grid grid-3"><img src="img/piano.png" alt="Untree.co"></div>
+				</div>
+			</div>
+			<div class="col-lg-5 ps-lg-5">
+				<h2 class="section-title mb-4"></h2>
+				<p>Tingkatkan kualitas musik Anda dengan alat musik berkualitas tinggi! Dapatkan guitar, drum, dan piano terbaik hanya di toko kami. Suara indah dan performa luar biasa menanti Anda!</p>
+				<ul class="list-unstyled custom-list my-4">
+					<li>Kualitas Terjamin</li>
+					<li>Harga Kompetitif</li>
+					<li>Pengiriman Cepat</li>
+					<li>Pelayanan Profesional</li>
+				</ul>
+				<p><a href="about.php" class="btn">Explore</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="products-section">
+	<div class="container">
+		<div class="text-products">
+			<div class="title-product col-7 col-sm-6 col-md-9">
+				<h2 class="text-main">TREND COLLECTION</h2>
+			</div>
+
+		</div>
+		<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<div class="cards-wrapper">
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar1.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar1.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar1.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar1.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="cards-wrapper">
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar2.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar2.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar2.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+						<div class="card" style="width: 18rem;">
+							<div class="image-wrapper">
+								<img src="img/gitar2.png" class="card-img-top" alt="...">
+							</div>
+							<div class="card-body">
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+
+	</div>
+</section>
+
+
+<div class="instrument-section">
+	<div class="container">
+		<div class="row justify-content-center">
+			<h1>KATEGORI</h1><br><br>
+			<div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+				<a class="instrument-item" href="shop">
+					<img src="img/guitar.png" class="img-fluid instrument-thumbnail">
+					<h3 class="instrument-title">Guitar</h3>
+				</a>
+			</div>
+			<div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+				<a class="instrument-item" href="shop">
+					<img src="img/drum-set.png" class="img-fluid instrument-thumbnail">
+					<h3 class="instrument-title">Drum</h3>
+
+				</a>
+			</div>
+			<div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+				<a class="instrument-item" href="shop">
+					<img src="img/grand-piano.png" class="img-fluid instrument-thumbnail">
+					<h3 class="instrument-title">Piano</h3>
+				</a>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+		</main>
+	</div>
+</section>
+
+<?php include 'footer.php'; ?>
